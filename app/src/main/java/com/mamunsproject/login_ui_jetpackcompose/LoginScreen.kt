1f7ccc22387ffeece6fbae4034c399375359e36e
Abdullah Al Mamun <aamamun25000@gmail.com>
2023-06-23 00:33:35 +0600

    Top Section

--- app/src/main/java/com/mamunsproject/login_ui_jetpackcompose/LoginScreen.kt
+++ app/src/main/java/com/mamunsproject/login_ui_jetpackcompose/LoginScreen.kt
@@ -1,8 +1,101 @@
 package com.mamunsproject.login_ui_jetpackcompose
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.mamunsproject.login_ui_jetpackcompose.ui.theme.Black
 
 @Composable
-fun LoginScreen(){
+fun LoginScreen() {
 
+    Surface {
+
+        Column(modifier = Modifier.fillMaxSize()) {
+            TopSection()
+        }
+    }
+
+}
+
+@Composable
+private fun TopSection() {
+    // to support light and dark
+    val uiColor = if (isSystemInDarkTheme()) Color.White else Black
+
+
+    Box(
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(fraction = 0.46f),
+            painter = painterResource(id = R.drawable.substrack),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds
+        )
+
+
+
+        Row(
+            modifier = Modifier.padding(top = 80.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                modifier = Modifier.size(42.dp),
+                painter = painterResource(id = R.drawable.t_logo),
+                contentDescription = stringResource(id = R.string.logo),
+                tint = uiColor
+            )
+            Spacer(modifier = Modifier.width(15.dp))
+            Column {
+                Text(
+                    text = stringResource(id = R.string.the_tolet),
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = uiColor
+                )
+
+                Text(
+                    text = stringResource(id = R.string.find_house),
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = uiColor
+                )
+
+
+            }
+
+        }
+
+
+        Text(
+            modifier = Modifier
+                .padding(bottom = 10.dp)
+                .align(alignment = Alignment.BottomCenter),
+            text = stringResource(id = R.string.login),
+            style = MaterialTheme.typography.headlineLarge,
+            color = uiColor
+        )
+
+    }
 }
